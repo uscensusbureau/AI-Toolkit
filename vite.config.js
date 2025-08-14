2fe556e7d6c2c673d9192bc83a3ebbb3785727b0
Curtis Mitchell <curtis.l.mitchell@census.gov>
2025-08-14 11:13:02 -0700

    Remove debugging console.log

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-console.log("process.env.BASE: ", process.env.BASE);
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.BASE,
