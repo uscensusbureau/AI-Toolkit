3c65f13ed7a7f842220651b17447e165ac418087
Curtis Mitchell <curtis.l.mitchell@census.gov>
2025-09-02 09:17:30 -0700

    Temp setting for Vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/AI-Toolkit/",
+  base: "/AI-Toolkit/pr-preview/pr-34/",
   plugins: [
     react(),
     tailwindcss()
